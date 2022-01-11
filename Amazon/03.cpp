cf96ad019aca6cb01c0b67f77c7c54e410e381c2
amantayal2909 <76427320+amantayal2909@users.noreply.github.com>
2022-01-11 23:39:00 +0530

    Update 03.cpp

--- Amazon/03.cpp
+++ Amazon/03.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Solution {
   public:
-    vector<int> max_of_subarrays(vector<int> arr, int n, int k) 
+    vector<int> max_subarrays(vector<int> arr, int n, int k) 
     {
         vector<int> ans;
         priority_queue<pair<int, int>> pq;
@@ -45,7 +45,7 @@ int main() {
         vector<int> arr(n);
         for (int i = 0; i < n; i++) cin >> arr[i];
         Solution ob;
-        vector<int> res = ob.max_of_subarrays(arr, n, k);
+        vector<int> res = ob.max_subarrays(arr, n, k);
         for (int i = 0; i < res.size(); i++) cout << res[i] << " ";
         cout << endl;
     }
